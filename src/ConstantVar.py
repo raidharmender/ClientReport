96ea7fd48071c1311347f38f14f4cfdce63461af
Dharmender Rai <dharmender.rai@gmail.com>
2023-12-13 08:02:52 +0800

    Updated files

---
+++ src/ConstantVar.py
@@ -0,0 +1,13 @@
+"""A class to cover all constants
+used in this project
+"""
+
+
+class ConstantVar:
+    """This class contains all the constants
+    used in this project
+    """
+    CFG_FILE = '../config/parse.json'
+    INPUT_FILE = "../data/input.txt"
+    CFG_FILE_ENC = "utf-8"
+    INPUT_FILE_ENC = "utf-8"
